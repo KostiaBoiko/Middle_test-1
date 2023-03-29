addeb51db56cf9d6f48dca3fd079304df2aa11ae
KostiaBoiko <boikokostia@knu.ua>
2023-03-29 14:07:57 +0300

    created files and initialized in program

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+"""
+Напишіть Python-скрипт, який зчитує вміст двох файлів з розширенням ".txt",
+порівнює їх та записує у файл з назвою "same.txt" всі рядки, які містяться в обох файлах,
+у файл "diff.txt" - рядки, які містяться лише в одному з двох файлів.
+"""
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+import os
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Вказуємо теку з файлами
+folder_path = "files"
+
+# Створюємо порожні списки для збереження рядків з файлів
+file1_lines = []
+file2_lines = []
+
