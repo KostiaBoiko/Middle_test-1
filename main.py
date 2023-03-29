2ef34835f10506159e7ab0d7e58bbaeaa4b5afef
KostiaBoiko <boikokostia@knu.ua>
2023-03-29 14:23:26 +0300

    created program class and created function to write same lines

--- main.py
+++ main.py
@@ -6,18 +6,28 @@
 
 import os
 
-# Вказуємо теку з файлами
-folder_path = "files"
+class Loader:
+    def __init__(self):
+        # Вказуємо теку з файлами
+        self.folder_path = "files"
 
-# Створюємо порожні списки для збереження рядків з файлів
-file1_lines = []
-file2_lines = []
 
-# Зчитуємо файли з теки з розширенням .txt
-for file_name in os.listdir(folder_path):
-    if file_name.endswith(".txt"):
-        with open(os.path.join(folder_path, file_name), "r") as f:
-            if file_name == "file_1.txt":
-                file1_lines = f.readlines()
-            elif file_name == "file_2.txt":
-                file2_lines = f.readlines()
+    # Зчитуємо файли з теки з розширенням .txt
+    def read_files(self):
+        for file_name in os.listdir(self.folder_path):
+            if file_name.endswith(".txt"):
+                with open(os.path.join(self.folder_path, file_name), "r") as f:
+                    if file_name == "file_1.txt":
+                        file1_lines = f.readlines()
+                    elif file_name == "file_2.txt":
+                        file2_lines = f.readlines()
+        return file1_lines, file2_lines
+
+
+    # Знаходимо рядки, які містяться в обох файлах
+    def write_same_lines(self, file1, file2):
+        same_lines = set(file1).intersection(file2)
+        return same_lines
+
+
+
