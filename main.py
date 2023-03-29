f22e1b333a2b383fa065f265242e6d5ddb94775c
KostiaBoiko <boikokostia@knu.ua>
2023-03-29 15:15:07 +0300

    added flake8

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-"""
-Напишіть Python-скрипт, який зчитує вміст двох файлів з розширенням ".txt",
-порівнює їх та записує у файл з назвою "same.txt" всі рядки, які містяться в обох файлах,
-у файл "diff.txt" - рядки, які містяться лише в одному з двох файлів.
-"""
-
 import os
+
 
 class Loader:
     def __init__(self):
-        # Вказуємо теку з файлами
         self.folder_path = "files"
 
-
-    # Зчитуємо файли з теки з розширенням .txt
     def read_files(self):
         for file_name in os.listdir(self.folder_path):
             if file_name.endswith(".txt"):
@@ -23,18 +15,14 @@ class Loader:
                         file2_lines = f.readlines()
         return file1_lines, file2_lines
 
-
-    # Знаходимо рядки, які містяться в обох файлах
     def write_same_lines(self, file1, file2):
         same_lines = set(file1).intersection(file2)
         return same_lines
 
-    # Знаходимо рядки, які містяться лише в одному з файлів
     def write_diff_lines(self, file1, file2):
         diff_lines = set(file1).symmetric_difference(file2)
         return diff_lines
 
-    # Записуємо результати в файли same.txt та diff.txt
     def write_to_files(self, samelines, difflines):
         with open(os.path.join(self.folder_path, "same.txt"), "w") as same_file:
             for line in samelines:
@@ -45,12 +33,8 @@ class Loader:
                 diff_file.write(line)
 
 
-
 loader = Loader()
 file_1, file_2 = loader.read_files()
 same_lines = loader.write_same_lines(file_1, file_2)
 diff_lines = loader.write_diff_lines(file_1, file_2)
 loader.write_to_files(same_lines, diff_lines)
-
-
-
--- tests/test_code.py
+++ tests/test_code.py
@@ -1,22 +1,27 @@
 import pytest
 from main import Loader
 
+
 @pytest.fixture()
 def loader():
     return Loader()
 
 
 @pytest.mark.parametrize("file_1, file_2, expected_result", [({"line_1", "line_2"}, {"line_1"}, {"line_1"}),
-                                            ({"line_1", "line_2"}, {"line_2"}, {"line_2"}),
-                                            ({"line_1", "line_2"}, {"line_1", "line_2"}, {"line_1", "line_2"})])
+                                                             ({"line_1", "line_2"}, {"line_2"}, {"line_2"}),
+                                                             ({"line_1", "line_2"}, {"line_1", "line_2"},
+                                                              {"line_1", "line_2"})])
 def test_same_lines(loader, file_1, file_2, expected_result):
     result = loader.write_same_lines(file_1, file_2)
     assert result == expected_result
 
 
-@pytest.mark.parametrize("file_1, file_2, expected_result", [({"line_1", "line_2"}, {"line_1", "line_3"}, {"line_2", "line_3"}),
-                                            ({"line_1", "line_3"}, {"line_2"}, {"line_1", "line_2", "line_3"}),
-                                            ({"line_1"}, {"line_3", "line_2", "line_1"}, {"line_2", "line_3"})])
+@pytest.mark.parametrize("file_1, file_2, expected_result", [({"line_1", "line_2"}, {"line_1", "line_3"}, {"line_2",
+                                                                                                           "line_3"}),
+                                                             ({"line_1", "line_3"}, {"line_2"}, {"line_1", "line_2",
+                                                                                                 "line_3"}),
+                                                             ({"line_1"}, {"line_3", "line_2", "line_1"}, {"line_2",
+                                                                                                           "line_3"})])
 def test_diff_lines(loader, file_1, file_2, expected_result):
     result = loader.write_diff_lines(file_1, file_2)
     assert result == expected_result
